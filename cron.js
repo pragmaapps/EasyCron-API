48481817f6240c47f532fd5cf76fb1a47c7b34cf
Harshit Jain <harshit.jain@ipragmatech.com>
2018-06-28 15:59:42 +0530

    Change state to changeState Add checks for cron id Fix typo

--- cron.js
+++ cron.js
@@ -18,7 +18,7 @@ function add(task, expression, API_URL) {
 		} = task;
 
 		if (minute === "*" && hour === "*" && day === "*" && month === "*") {
-			reject(new Error("Provide atleast minute, hour, day or month in input"));
+			reject(new Error("Provide at least minute, hour, day or month in input"));
 		}
 		let queryString;
 		if (expression) {
@@ -118,25 +118,28 @@ function add(task, expression, API_URL) {
 	});
 }
 
-function state (task = {}, API_URL) {
+function changeState (task = {}, API_URL) {
 	return new Promise((resolve, reject) => {
-	let {
-		id = ''
-	} = task;
-	got.get(API_URL + "&id=" + id)
-		.then(res => {
-			const response = JSON.parse(res.body);
-			if (response.error) {
-				reject(response.error);
-			}
-			resolve(response);
-		})
-		.catch(error => {
-			if (error.name) {
-				error.message = error.name;
-			}
-			reject(error);
-		});
+		let { id = null } = task;
+		if(id === null){
+			reject(new Error("Cron id not received in input"));
+		} else if (!((typeof id === "string" && id.length > 0) || typeof id === "number")) {
+			reject(new Error("Invalid cron id received in input"));
+		}
+		got.get(API_URL + "&id=" + id)
+			.then(res => {
+				const response = JSON.parse(res.body);
+				if (response.error) {
+					reject(response.error);
+				}
+				resolve(response);
+			})
+			.catch(error => {
+				if (error.name) {
+					error.message = error.name;
+				}
+				reject(error);
+			});
 	});
 }
 
@@ -150,9 +153,9 @@ function easyCron(config = {}) {
 	return {
 		add: (task) => add(task, false, API_URL + "add?token=" + token),
 		addCronExp: (task) => add(task, true, API_URL + "add?token=" + token),
-		enable: (task) => state (task, API_URL + "enable?token=" + token),
-		disable: (task) => state (task, API_URL + "disable?token=" + token),
-		delete: (task) => state (task, API_URL + "delete?token=" + token)
+		enable: (task) => changeState (task, API_URL + "enable?token=" + token),
+		disable: (task) => changeState (task, API_URL + "disable?token=" + token),
+		delete: (task) => changeState (task, API_URL + "delete?token=" + token)
 	}
 
 };
